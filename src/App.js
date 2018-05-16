b94f045d0a7134998295f68d30395cc02e3d15b3
Salomao Rodrigues <salomao88@gmail.com>
2018-05-17 00:04:02 +0100

    Add Map to allow selecting a location

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import Map from "./Map";
+
 import logo from './logo.svg';
 import './App.css';
+const googleMapURL = "https://maps.googleapis.com/maps/api/js?&v=3.exp&libraries=geometry,drawing,places";
 
 class App extends Component {
+  state = {
+    unsavedKey: "",
+    apiKey: null
+  };
+
   render() {
+    const { unsavedKey, apiKey } = this.state;
+    console.log(this.state);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Google Places to Mongo 2dsphere</h1>
         </header>
+
+        { apiKey && <Map googleMapURL={`${googleMapURL}&=${apiKey}`}/> }
+        <label>
+          API Key
+          <input
+            type="password"
+            onChange={ ({ target }) => this.setState({ unsavedKey: target.value }) }
+            name="apikey" value={ unsavedKey }/>
+          <button onClick={ () => this.setState({ apiKey: unsavedKey })}>Update</button>
+        </label>
       </div>
     );
   }
---
+++ src/Map/Map.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+import { compose, withProps } from "recompose";
+import { withGoogleMap, withScriptjs, GoogleMap, Marker } from "react-google-maps";
+
+class Map extends React.Component {
+  render() {
+    return <Fragment>
+      <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+      >
+        {<Marker position={{ lat: -34.397, lng: 150.644 }} />}
+      </GoogleMap>
+    </Fragment>;
+  }
+}
+
+export default compose(
+  withProps({
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)(Map);
